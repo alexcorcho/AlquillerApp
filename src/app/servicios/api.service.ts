0999a7feabe62235275c299ffed283f6b493a119
Alexander Corcho <alexander.corcho@gmail.com>
2021-04-11 00:33:18 -0500

    comentado API

--- src/app/servicios/api.service.ts
+++ src/app/servicios/api.service.ts
@@ -1,16 +1,17 @@
 import { Injectable, NgZone } from '@angular/core';
-import { HttpClient} from '@angular/common/http'
+import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-    private _apiLocalURL ='../../assets/API-CORCHO/API_LOCAL.json';
+    // tslint:disable-next-line: variable-name
+    private _apiLocalURL = '../../assets/API-CORCHO/API_LOCAL.json';
   constructor(private Http: HttpClient) { }
 
-  getJson():Observable<any>{
-    return this.Http.get(this._apiLocalURL)
+  getJson(): Observable <any>{
+    return this.Http.get(this._apiLocalURL );
   }
 
 
